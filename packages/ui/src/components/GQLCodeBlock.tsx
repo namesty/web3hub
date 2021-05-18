aa8a8ad477948e9e4ca37ce21933844aafeb72fa
Jeff Ward <jsward.17@gmail.com>
2021-05-18 13:43:09 -0400

    stumped

--- packages/ui/src/components/GQLCodeBlock.tsx
+++ packages/ui/src/components/GQLCodeBlock.tsx
@@ -4,19 +4,93 @@ import { jsx, Styled } from 'theme-ui'
 import 'prism-solarized-dark/prism-solarizeddark.css'
 import Prism from 'prismjs'
 import 'prismjs/components/prism-graphql'
-import { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 type GQLCodeBlockProps = {
   title: string
   value: any
 }
 
+const nextUntil = function (elem, selector, filter) {
+  const siblings = []
+  elem = elem.nextElementSibling
+  while (elem) {
+    if (elem.matches(selector)) break
+    if (filter && !elem.matches(filter)) {
+      elem = elem.nextElementSibling
+      continue
+    }
+    siblings.push(elem)
+    elem = elem.nextElementSibling
+  }
+  return siblings
+}
+
 const GQLCodeBlock = ({ title, value }: GQLCodeBlockProps) => {
+  const codeEl = useRef(null)
+
+  // Do Syntax highlight
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      Prism.highlightAll()
+      Prism.highlightAllUnder(codeEl.current)
     }
   }, [])
+
+  // Inject collapse btns and start marker
+  useEffect(() => {
+    setTimeout(() => {
+      let importsElement: any = Array.from(
+        codeEl.current.querySelectorAll(`.token.directive.function`),
+      ).find((el: any) => el.textContent.includes('@import'))
+      if (importsElement) {
+        let marker = document.createElement('span')
+        let btn = document.createElement('button')
+        marker.className = 'start-marker'
+        btn.textContent = '-'
+        btn.className = 'type-toggle'
+        importsElement.before(btn)
+        importsElement.after(marker)
+      }
+    }, 1000)
+  }, [])
+
+  // Check for "parans"
+  useEffect(() => {
+    setTimeout(() => {
+    let closeParenElement: any = Array.from(
+      codeEl.current.querySelectorAll(`.token.punctuation`),
+    ).filter((el: any) => el.textContent === ')')[0]
+    console.log(closeParenElement)
+    if (closeParenElement) {
+      let marker = document.createElement('span')
+      marker.className = 'end-marker'
+      marker.textContent = ''
+      closeParenElement.after(marker)
+    }
+  },1100)
+  }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      let btn = codeEl.current.querySelector(`.type-toggle`)
+      if (btn !== null) {
+        btn.addEventListener('click', () => {
+          const start = codeEl.current.querySelector('.start-marker')
+          const inbetweenEls = nextUntil(start, '.end-marker', null)
+          inbetweenEls.map((el) => {
+            if (el.classList.contains('hidden')) {
+              el.classList.remove('hidden')
+              btn.innerHTML = '-'
+            } else {
+              el.classList.add('hidden')
+              btn.innerHTML = '+'
+            }
+          })
+        })
+      }
+    }, 1200)
+  }, [])
+
   return (
     <div
       sx={{
@@ -24,14 +98,31 @@ const GQLCodeBlock = ({ title, value }: GQLCodeBlockProps) => {
           fontSize: '12px',
           lineHeight: 1.2,
           borderRadius: 0,
-          mb: 0
+          mb: 0,
         },
+        '.type-toggle': {
+          color: 'white',
+          background: '#333',
+          border: 'none',
+          borderRadius: '4px',
+          mr: 1,
+        },
+        '.hidden': {
+          display: 'none',
+        },
+        button: {
+          width: '20px'
+        },
+        code: {
+          // height: 100px;
+          // white-space: initial;
+        }
       }}
     >
       <Styled.h5 sx={{ m: 0, py: 2, bg: 'white' }}>{title}</Styled.h5>
       <pre className="language-graphql">
-        <code>
-          {value.map((item, idx) => {
+        <code ref={codeEl}>
+          {value.map((item) => {
             return item + '\n\n'
           })}
         </code>
--- packages/ui/src/pages/test.tsx
+++ packages/ui/src/pages/test.tsx
@@ -6,8 +6,10 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import BGWave from '../components/BGWave'
 import BottomSpace from '../components/BottomSpace'
-import GQLCodeBlock from '../components/GQLCodeBlock'
 import cleanSchema from '../utils/cleanSchema'
+
+import dynamic from 'next/dynamic'
+const GQLCodeBlock = dynamic(() => import('../components/GQLCodeBlock'), { ssr: false })
 
 const schemaExample =
   '### Web3API Header START ###\nscalar UInt\nscalar UInt8\nscalar UInt16\nscalar UInt32\nscalar UInt64\nscalar Int\nscalar Int8\nscalar Int16\nscalar Int32\nscalar Int64\nscalar Bytes\n\ndirective @imported(\n  uri: String!\n  namespace: String!\n  nativeType: String!\n) on OBJECT | ENUM\n\ndirective @imports(\n  types: [String!]!\n) on OBJECT\n### Web3API Header END ###\n\ntype Query @imports(\n  types: [\n    "Ethereum_Query"\n  ]\n) {\n  getData(\n    address: String!\n  ): UInt32!\n}\n\ntype Mutation @imports(\n  types: [\n    "Ethereum_Mutation"\n  ]\n) {\n  setData(\n    options: SetDataOptions!\n  ): SetDataResult!\n\n  deployContract: String!\n}\n\ntype SetDataOptions {\n  address: String!\n  value: UInt32!\n}\n\ntype SetDataResult {\n  txReceipt: String!\n  value: UInt32!\n}\n\n### Imported Queries START ###\n\ntype Ethereum_Query @imported(\n  uri: "w3://ens/ethereum.web3api.eth",\n  namespace: "Ethereum",\n  nativeType: "Query"\n) {\n  callView(\n    address: String!\n    method: String!\n    args: [String!]!\n  ): String!\n}\n\ntype Ethereum_Mutation @imported(\n  uri: "w3://ens/ethereum.web3api.eth",\n  namespace: "Ethereum",\n  nativeType: "Mutation"\n) {\n  sendTransaction(\n    address: String!\n    method: String!\n    args: [String!]!\n  ): String!\n\n  deployContract(\n    abi: String!\n    bytecode: String!\n  ): String!\n}\n\n### Imported Queries END ###\n\n### Imported Objects START ###\n\n### Imported Objects END ###\n'
